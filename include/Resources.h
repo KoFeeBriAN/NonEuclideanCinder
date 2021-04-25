6c11967343eeec99304c0abb7fd630cc590adcbf
KoFeeBriAN <40123144+TuwanonKonjaiboon@users.noreply.github.com>
2021-04-26 00:07:59 +0900

    setup: init project

--- include/Resources.h
+++ include/Resources.h
@@ -1,7 +1,8 @@
 #pragma once
 #include "cinder/CinderResources.h"
 
-//#define RES_MY_RES			CINDER_RESOURCE( ../resources/, image_name.png, 128, IMAGE )
+#define RES_ENV_MAP				CINDER_RESOURCE( ../resources/environment_maps/, humus_sf.jpg, 128, IMAGE  )
+
 
 
 
